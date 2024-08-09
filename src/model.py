b51fff09227d41ca00574e81ccc2442da01a3806
Nimsara De Zoysa <105286867+nimdz@users.noreply.github.com>
2024-08-09 19:33:18 +0530

    final commit

---
+++ src/model.py
@@ -0,0 +1,14 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.activations import relu, softmax
+
+def create_model():
+    model = Sequential([
+        Flatten(input_shape=(28, 28)),
+        Dense(128, activation=relu),
+        Dense(10, activation=softmax)
+    ])
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+    return model
